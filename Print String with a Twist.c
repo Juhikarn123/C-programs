d3b7cb94a6bd0aeb430872de9ea81d0884f7c077
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-27 14:07:05 +0530

    Create Print String with a Twist.c

---
+++ Print String with a Twist.c
@@ -0,0 +1,15 @@
+Print String with a Twist
+
+Write a program to print "Welcome to the World of "C" Programming", with the C in quotes.
+
+Input:
+None
+
+Output:
+Welcome to the World of "C" Programming
+  ------------------------------------------------------
+  #include<stdio.h>
+int main()
+{
+    printf("Welcome to the World of \"C\" Programming");
+}
