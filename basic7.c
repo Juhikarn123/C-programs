3a4f100e9dc8bf1aa1321ccf594f4ce491050d13
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-27 13:51:37 +0530

    Create basic7.c

---
+++ basic7.c
@@ -0,0 +1,31 @@
+Product of 2 numbers
+
+Write a program to accept 2 numbers as input and print their product.
+
+Input:
+2 Integers
+
+Output:
+Integer
+
+Sample Input 1
+10 20
+
+Sample Output 1
+200
+
+Sample Input 2
+100 900
+
+Sample Output 2
+90000
+
+-------------------------------------------
+  #include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    printf("%d",c);
+}
