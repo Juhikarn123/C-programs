1f6df9081ae5bfb0f8fb4b60bb08ffe9afbd236d
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-27 14:10:08 +0530

    Create Print \r.c

---
+++ Print \r.c
@@ -0,0 +1,18 @@
+Print \r
+
+Problem Statement:
+Write a program to print \r.
+
+Input:
+None
+
+Output:
+\r
+
+----------------------------------------------------
+
+  #include<stdio.h>
+int main()
+{
+    printf("\\r\n");
+}
