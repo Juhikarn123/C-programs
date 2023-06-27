e9f899e7a649bf0e37847f99fabbb49ae52d4b3f
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-27 13:50:32 +0530

    Create basic6.c

---
+++ basic6.c
@@ -0,0 +1,28 @@
+Write a program to accept 2 numbers as input.
+
+Input:
+2 Integers
+
+Output:
+Integer
+
+Sample Input 1
+10 20
+
+Sample Output 1
+30
+
+Sample Input 2
+100 900
+
+Sample Output 2
+1000
+-----------------------------------------------------------
+  #include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("%d",c);
+}
