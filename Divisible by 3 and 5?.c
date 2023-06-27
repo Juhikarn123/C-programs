b6edbce92865591d2fd2e5ec2faea94c065a42f7
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-27 14:30:49 +0530

    Create Divisible by 3 and 5?.c

---
+++ Divisible by 3 and 5?.c
@@ -0,0 +1,41 @@
+Divisible by 3 and 5?
+
+Problem Statement:
+
+Write a program to accept a number as input and check if it is divisible by 3 as well as 5.
+It the number is divisible print YES else print NO.
+
+Input:
+Integer
+
+Output:
+YES or NO
+
+Constraints
+1<=num<=2,147,483,647
+
+Sample Input 1
+15
+
+Sample Output 1
+YES
+
+Sample Input 2
+2
+
+Sample Output 2
+NO
+
+------------------------
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%3==0 && a%5==0)
+    {
+        printf("YES");
+    }
+    else
+    printf("NO");
+}
