ba5907e817024217bdbd2b9301c10d7e0c266fd0
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-27 14:56:41 +0530

    Create Number Pattern3.c

---
+++ Number Pattern3.c
@@ -0,0 +1,37 @@
+Number Pattern
+
+Write a program to accept a number N as input from the user and print the following pattern.
+
+Sample input 1
+5
+
+Sample output 1
+54321
+4321
+321
+21
+1
+
+Sample input 2
+4
+
+Sample output 2
+4321
+321
+21
+1
+---------------------------------------
+  #include<stdio.h>
+int main()
+{
+    int i,j,row;
+    scanf("%d",&row);
+    for(i=row;i>=1;i--)
+    {
+        for(j=i;j>0;j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}
