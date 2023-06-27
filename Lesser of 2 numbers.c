72649dad2a400b357c4266d9899539f9c07f4fcc
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-27 14:14:50 +0530

    Create Lesser of 2 numbers.c

---
+++ Lesser of 2 numbers.c
@@ -0,0 +1,43 @@
+Lesser of 2 numbers
+
+Problem Statement:
+
+Write a program to accept 2 numbers as input and print the lesser of the 2 numbers.
+You may assume that the numbers will not be equal.
+
+Input:
+Integer
+
+Output:
+Integer
+
+Constraints
+1<=num<=2,147,483,647
+
+Sample Input 1
+6789 6790
+
+Sample Output 1
+6789
+
+Sample Input 2
+0 1
+
+Sample Output 2
+0
+
+--------------------------------------------------------
+  #include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a<b)
+    {
+        printf("%d",a);
+    }
+    else
+    {
+        printf("%d",b);
+    }
+}
